11e93d284099cc71a5fb38220a35802a4e256fb1
greenfluorite <287539987+greenfluorite@users.noreply.github.com>
2026-06-08 14:44:21 +0800

    Update P3_Bowels_of_Agony.cs

--- SplatoonScripts/Duties/Dawntrail/Dancing Mad/P3_Bowels_of_Agony.cs
+++ SplatoonScripts/Duties/Dawntrail/Dancing Mad/P3_Bowels_of_Agony.cs
@@ -1,8 +1,9 @@
 using Dalamud.Game.ClientState.Objects.SubKinds;
 using ECommons;
+using ECommons.DalamudServices;
 using ECommons.GameFunctions;
+using ECommons.Hooks.ActionEffectTypes;
 using ECommons.MathHelpers;
-using FFXIVClientStructs.FFXIV.Client.Game;
 using Splatoon;
 using Splatoon.SplatoonScripting;
 using System;
@@ -17,7 +18,7 @@ public unsafe class P3_Bowels_of_Agony : SplatoonScript
     public override Metadata Metadata { get; } = new(1, "greenfluorite");
     public override HashSet<uint>? ValidTerritories { get; } = [1363];
 
-    // Crystal DataIDs (decimal)
+    // Crystal DataIDs
     public uint DataID_Fire  = 966714;
     public uint DataID_Water = 966715;
     public uint DataID_Wind  = 966716;
@@ -34,7 +35,7 @@ public unsafe class P3_Bowels_of_Agony : SplatoonScript
     // Arena center
     static readonly Vector2 Center = new Vector2(100f, 100f);
 
-    // Offset: supports go outward, DPS go inward
+    // Offset: positive = away from center, negative = toward center
     public float SupportOffset = 3.0f;
     public float DpsOffset     = -2.0f;
 
@@ -42,16 +43,17 @@ public unsafe class P3_Bowels_of_Agony : SplatoonScript
 
     public override void OnSetup()
     {
-        for (int i = 0; i < 8; i++)
+        for(int i = 0; i < 8; i++)
         {
-            Controller.RegisterElementFromCode("Marker" + i.ToString(),
-                "{\"Name\":\"\",\"type\":1,\"radius\":0.8,\"color\":3355508735,\"Filled\":true,\"fillIntensity\":0.4,\"overlayVOffset\":1.5,\"thicc\":3.0,\"overlayText\":\"GO HERE\",\"refActorComparisonType\":2}");
+            Controller.RegisterElementFromCode($"Marker{i}", """
+                {"Name":"","type":1,"radius":0.8,"color":3355508735,"Filled":true,"fillIntensity":0.4,"overlayVOffset":1.5,"thicc":3.0,"overlayText":"GO HERE","refActorComparisonType":2}
+                """);
         }
     }
 
     public override void OnActionEffectEvent(ActionEffectSet set)
     {
-        if (set.Action != null && set.Action.Value.RowId == Action_BowelsOfAgony)
+        if(set.Action != null && set.Action.Value.RowId == Action_BowelsOfAgony)
         {
             _active = true;
         }
@@ -60,23 +62,31 @@ public unsafe class P3_Bowels_of_Agony : SplatoonScript
     public override void OnUpdate()
     {
         Controller.Hide();
-        if (!_active) return;
+        if(!_active) return;
 
-        var windCrystal  = GetObjectByDataID(DataID_Wind);
-        var fireCrystal  = GetObjectByDataID(DataID_Fire);
-        var waterCrystal = GetObjectByDataID(DataID_Water);
+        // Find crystals by DataID using Svc.Objects
+        IGameObject windCrystal  = null;
+        IGameObject fireCrystal  = null;
+        IGameObject waterCrystal = null;
 
-        if (windCrystal == null || fireCrystal == null || waterCrystal == null) return;
+        foreach(var obj in Svc.Objects)
+        {
+            if(obj.DataId == DataID_Wind)  windCrystal  = obj;
+            if(obj.DataId == DataID_Fire)  fireCrystal  = obj;
+            if(obj.DataId == DataID_Water) waterCrystal = obj;
+        }
 
-        var windPos  = new Vector2(windCrystal->Position.X,  windCrystal->Position.Z);
-        var firePos  = new Vector2(fireCrystal->Position.X,  fireCrystal->Position.Z);
-        var waterPos = new Vector2(waterCrystal->Position.X, waterCrystal->Position.Z);
+        if(windCrystal == null || fireCrystal == null || waterCrystal == null) return;
+
+        var windPos  = new Vector2(windCrystal.Position.X,  windCrystal.Position.Z);
+        var firePos  = new Vector2(fireCrystal.Position.X,  fireCrystal.Position.Z);
+        var waterPos = new Vector2(waterCrystal.Position.X, waterCrystal.Position.Z);
 
         int idx = 0;
         var members = Controller.GetPartyMembers();
-        for (int i = 0; i < members.Count; i++)
+        for(int i = 0; i < members.Count; i++)
         {
-            var member   = members[i];
+            var member    = members[i];
             bool isSupport = (i < 4);
 
             bool hasEntropy      = member.StatusList.Any(s => s.StatusId == Buff_Entropy);
@@ -84,35 +94,24 @@ public unsafe class P3_Bowels_of_Agony : SplatoonScript
             bool hasWindDebuff   = member.StatusList.Any(s => s.StatusId == Buff_Headwind || s.StatusId == Buff_Tailwind);
 
             Vector2 crystalPos;
-            if      (hasEntropy)      crystalPos = firePos;
-            else if (hasDynamicFluid) crystalPos = waterPos;
-            else if (hasWindDebuff)   crystalPos = windPos;
+            if     (hasEntropy)      crystalPos = firePos;
+            else if(hasDynamicFluid) crystalPos = waterPos;
+            else if(hasWindDebuff)   crystalPos = windPos;
             else continue;
 
             var dir    = Vector2.Normalize(crystalPos - Center);
             float off  = isSupport ? SupportOffset : DpsOffset;
             var target = crystalPos + dir * off;
 
-            if (Controller.TryGetElementByName("Marker" + idx.ToString(), out var el))
+            if(Controller.TryGetElementByName($"Marker{idx}", out var el))
             {
-                el.Enabled     = true;
-                el.RefPosition = new Vector3(target.X, 0f, target.Y);
+                el.Enabled          = true;
+                el.RefPosition      = new Vector3(target.X, 0f, target.Y);
                 el.refActorObjectID = member.ObjectId;
-                el.tether      = true;
+                el.tether           = true;
             }
             idx++;
         }
-    }
-
-    FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject* GetObjectByDataID(uint dataId)
-    {
-        for (int i = 0; i < 200; i++)
-        {
-            var obj = (FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject*)FFXIVClientStructs.FFXIV.Client.Game.Object.GameObjectManager.Instance()->Objects.IndexSorted[i].Value;
-            if (obj == null) continue;
-            if (obj->DataID == dataId) return obj;
-        }
-        return null;
     }
 
     public override void OnReset()
